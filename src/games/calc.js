f3eceeb670a732877df3bff0be7e4144339da343
tigp <hnztkfc@gmail.com>
2022-02-25 15:50:41 +0300

    fixed file

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,37 +2,37 @@ import getRandomNumber from '../random-number.js';
 import game from '../index.js';
 
 const generateDate = () => {
-    const num1 = getRandomNumber();
-    const num2 = getRandomNumber();
-    const operators = ['+', '-', '*'];
-    const indexOfOperation = getRandomNumber(0, operators.length - 1);
-    const question = `${num1} ${operators[indexOfOperation]} ${num2}`;
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const operators = ['+', '-', '*'];
+  const indexOfOperation = getRandomNumber(0, operators.length - 1);
+  const question = `${num1} ${operators[indexOfOperation]} ${num2}`;
 
-    let result = 0;
+  let result = 0;
 
-    switch (indexOfOperation) {
-        case 0:
-          result = num1 + num2;
-          break;
-        case 1:
-          result = num1 - num2;
-          break;
-        case 2:
-          result = num1 * num2;
-          break;
-        default:
-          break;
-    }
+  switch (indexOfOperation) {
+    case 0:
+      result = num1 + num2;
+      break;
+    case 1:
+      result = num1 - num2;
+      break;
+    case 2:
+      result = num1 * num2;
+      break;
+    default:
+      break;
+  }
 
-    const answer = result.toString();
+  const answer = result.toString();
 
-    return [question, answer];
+  return [question, answer];
 };
 
 const calc = () => {
-    const rulesMessage = 'What is the result of the expression?';
-    const roundsCount = 3;
-    game(rulesMessage, roundsCount, generateDate);
+  const rulesMessage = 'What is the result of the expression?';
+  const roundsCount = 3;
+  game(rulesMessage, roundsCount, generateDate);
 };
 
 export default calc;
