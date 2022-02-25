3b9315d091053085c9efb74252bd429c40e34579
tigp <hnztkfc@gmail.com>
2022-02-25 15:51:30 +0300

    fixed file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import readlineSync from 'readline-sync';
 
 const game = (rulesMessage, roundsCount, generateData) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May i have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(rulesMessage);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rulesMessage);
 
-    for (let i = 0; i < roundsCount; i += 1) {
-        const [question, answer] = generateData();
-        console.log(`Question: ${question}`);
-        const userAnswer = readlineSync.question('Your answer: ');
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = generateData();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-        if (userAnswer === answer) {
-            console.log('Correct!');
-        } else {
-            return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
-        }
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
     }
+  }
 
-    console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default game;
