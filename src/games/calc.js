365b75ffcc96f752f6a27370b91555213d74ffee
tigp <hnztkfc@gmail.com>
2022-02-25 14:27:29 +0300

    create calc game

---
+++ src/games/calc.js
@@ -0,0 +1,38 @@
+import getRandomNumber from '../random-number.js';
+import game from '../index.js';
+
+const generateDate = () => {
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    const operators = ['+', '-', '*'];
+    const indexOfOperation = getRandomNumber(0, operators.length - 1);
+    const question = `${num1} ${operators[indexOfOperation]} ${num2}`;
+
+    let result = 0;
+
+    switch (indexOfOperation) {
+        case 0:
+          result = num1 + num2;
+          break;
+        case 1:
+          result = num1 - num2;
+          break;
+        case 2:
+          result = num1 * num2;
+          break;
+        default:
+          break;
+    }
+
+    const answer = result.toString();
+
+    return [question, answer];
+};
+
+const calc = () => {
+    const rulesMessage = 'What is the result of the expression?';
+    const roundsCount = 3;
+    game(rulesMessage, roundsCount, generateDate);
+};
+
+export default calc;
