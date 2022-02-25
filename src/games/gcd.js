f897d9f74ce5cae038abc6a3cb14c1bca9bcb6a8
tigp <hnztkfc@gmail.com>
2022-02-25 15:53:37 +0300

    create greatest common divisor game wow!

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import getRandomNumber from '../random-number.js';
+import game from '../index.js';
+
+const generateDate = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const question = `${num1} ${num2}`;
+
+  const divisor = (a, b) => !b ? a : divisor(b, a % b);
+
+  const answer = divisor(num1, num2).toString();
+
+  return [question, answer];
+};
+
+const gcd = () => {
+  const rulesMessage = 'Find the greatest common divisor of given numbers.';
+  const roundsCount = 3;
+  game(rulesMessage, roundsCount, generateDate);
+};
+
+export default gcd;
