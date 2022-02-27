13489578c0e15988b89b137c83d683dd8c6edf2e
tigp <hnztkfc@gmail.com>
2022-02-27 16:00:31 +0300

    exe file progression game

---
+++ src/games/progression.js
@@ -0,0 +1,28 @@
+import getRandomNumber from '../random-number.js';
+import game from '../index.js';
+
+const generateData = () => {
+  const progression = [];
+  let current = getRandomNumber();
+  const progressionStep = getRandomNumber(7, 20);
+
+  for (let i = 0; i < 10; i += 1) {
+    progression.push(current);
+    current += progressionStep;
+  }
+
+  const missingIndex = getRandomNumber(0, progression.length - 1);
+  const answer = progression[missingIndex].toString();
+  progression[missingIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, answer];
+};
+
+const progression = () => {
+  const rulesMessage = 'What number is missing in the progression?';
+  const roundsCount = 3;
+  game(rulesMessage, roundsCount, generateData);
+};
+
+export default progression;
