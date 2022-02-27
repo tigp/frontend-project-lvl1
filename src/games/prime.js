284e491f234fa3a5966ff5c5dfc8dd19c2d34cf0
tigp <hnztkfc@gmail.com>
2022-02-27 22:27:54 +0300

    make exe file brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import game from '../index.js';
+import getRandomNumber from '../random-number.js';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const generateData = () => {
+  const num = getRandomNumber();
+  const question = num.toString();
+  const answer = isPrime(num) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+const prime = () => {
+  const rulesMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const roundsCount = 3;
+  game(rulesMessage, roundsCount, generateData);
+};
+
+export default prime;
